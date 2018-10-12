1ff8265a5a8e08ab095a15e8d67a08975ad489a0
xxf@qq.com <xxf@qq.com>
2018-10-12 14:41:45 +0800

    修改语法问题

--- src/main/java/com/drelephant/elephantadmin/business/basedata/controller/BdAreaRegionController.java
+++ src/main/java/com/drelephant/elephantadmin/business/basedata/controller/BdAreaRegionController.java
@@ -2,7 +2,9 @@ package com.drelephant.elephantadmin.business.basedata.controller;
 
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
