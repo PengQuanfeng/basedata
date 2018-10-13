718434438413f0755af5c45ed278370605245866
zhouchunlai <zhouchunlai@djinfo.cn>
2018-10-13 18:17:22 +0800

    提交无关引用

--- src/main/java/com/drelephant/elephantadmin/business/basedata/controller/BdServiceConfigController.java
+++ src/main/java/com/drelephant/elephantadmin/business/basedata/controller/BdServiceConfigController.java
@@ -1,8 +1,5 @@
 package com.drelephant.elephantadmin.business.basedata.controller;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
